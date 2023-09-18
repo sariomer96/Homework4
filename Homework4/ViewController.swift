e1edf62b5de1771b510bf5f3b4287dc548bd9326
omer <somersari.96@gmail.com>
2023-09-18 13:02:01 +0300

    Initial Commit

---
+++ Homework4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework4
+//
+//  Created by Omer on 18.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
